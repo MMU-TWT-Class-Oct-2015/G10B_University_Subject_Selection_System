e076fd45b1b2a13357867aa810741fd700464781
jiayan1026 <jiayan_1026@hotmail.com>
2016-02-04 15:58:46 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -17,26 +17,49 @@
 <html>
 	<head>
 	<title>Admin index</title>
-	<link rel="stylesheet" href="../design.css" type="text/css" />
+	<link rel="stylesheet" href="include/style.css" type="text/css" />
 </head>
 
-<body id="backcolor">
-<div id="header2" style="margin-left:auto;margin-right:auto;width:1520px;">
+<body>
+	<div id="header">
+		<div class="inHeader">
+			<div class="mosAdmin">
+				<?php $name = $_SESSION['aid']; ?>
+				Hello, <?php echo $name; ?><br>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+
+	<div id="wrapper">
+		<div id="leftBar">
+			<ul>
+				<p>&nbsp;</p>
+				<li> <a href="index.php">Home</a></li>
+				<li><a href="edit_pro.php">Edit Profile</a></li>
+				<li><a href="add_sub.php">Add Subjects</a></li>
+				<li><a href="view_sub.php">View Subjects</a></li>
+				<li><a href="register.php"> Register Student</a></li>
+				<li><a href="user.php">User</a></li>
+				<li><a href="logout.php">Logout</a></li>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+			</ul>   
+		</div>
+		<div id="rightContent">
+			
+		</div>
+		<div class="clear"></div>
+		<div id="footer">
+		
+		</div>
+	</div>
+
 			<a href="index.php"><img src="../picture/logo.png" width="205" height="73"></a>
 
-	<nav>
-	<ul>
-			<li><a href="index.php">Admin Index</a></li>
-			<li><a href="edit_pro.php">Edit Profile</a></li>
-			<li><a href="add_sub.php">Add Subjects</a></li>
-			<li><a href="view_sub.php">View Subjects</a></li>
-	</ul>
-	</nav>
-	<span style="text-align:right;float:right;color:#FFFFFF;margin-top:-10px;font-size:25px;" >
-			<p style="margin-right:20px;">Welcome, <?php echo $row["Admin_Name"]?>
-			||<a href="register.php"> Register</a> 
-			<input type="submit" name="logoutbtn" value="Logout" onclick="window.location='logout.php';"/></p>
-</div>
+	
 
 </body>
 </html>
