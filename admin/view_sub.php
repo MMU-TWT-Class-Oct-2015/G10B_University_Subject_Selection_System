b03ce493d489c84cd1b5e3b2d2329b7b70c672a4
Tan Jing Xian <1121115994@student.mmu.edu.my>
2016-02-11 21:03:38 +0800

    Update view_sub.php

--- admin/view_sub.php
+++ admin/view_sub.php
@@ -1,69 +1,93 @@
 <?php
 	include("../dataconn.php");
-
 	$sess_aid=$_SESSION["aid"];
-
-	$result = mysql_query("select * from subject");
-
+	$sql = mysql_query("select * from admin where Admin_ID = $sess_aid");
+	$rows = mysql_fetch_assoc($sql);
    if (!$sess_aid) {
 		header('Location: login.php');
 		exit();
 	}
-
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
 	<title>Admin index</title>
-	<link rel="stylesheet" href="../design.css" type="text/css" />
+	<link rel="stylesheet" href="include/style.css" type="text/css" />
 </head>
 
-<body id="backcolor">
-	<div id="header2" style="margin-left:auto;margin-right:auto;width:1520px;">
-			<a href="index.php"><img src="../picture/logo.png" width="205" height="73"></a>
-
-	<nav>
-	<ul>
-			<li><a href="index.php">Admin Index</a></li>
-			<li><a href="edit_pro.php">Edit Profile</a></li>
-			<li><a href="add_sub.php">Add Subjects</a></li>
-			<li><a href="view_sub.php">View Subjects</a></li>
-	</ul>
-	</nav>
-	<span style="text-align:right;float:right;color:#FFFFFF;margin-top:-10px;font-size:25px;" >
-			<p style="margin-right:20px;">Welcome, <?php echo $row["Admin_Name"]?>
-			||<a href="register.php"> Register</a> 
-			<input type="submit" name="logoutbtn" value="Logout" onclick="window.location='logout.php';"/></p>
-</div>
-
-
-			<table border="1" width="500px">
+<body >
+	<div id="header">
+		<div class="inHeader">
+			<div class="mosAdmin">
+				<?php $name = $_SESSION['aid']; ?>
+				Hello, <?php echo $name; ?><br>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+	<div id="wrapper">
+		<div id="leftBar">
+			<ul>
+				<p>&nbsp;</p>
+				<li> <a href="index.php">Home</a></li>
+				<li><a href="edit_pro.php">Edit Profile</a></li>
+				<li><a href="add_sub.php">Add Subjects</a></li>
+				<li><a href="view_sub.php">View Subjects</a></li>
+				<li><a href="register.php"> Register Student</a></li>
+				<li><a href="user.php">User</a></li>
+				<li><a href="logout.php">Logout</a></li>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+			</ul>
+		</div>
+		<div id="rightContent">
+			<table width="100%" border="0" cellspacing="0" cellpadding="20">
 				<tr>
-					<th>Subject Code</th>
-					<th>Subject Title</th>
-					<th>Subject Date</th>
-					<th>Subject Time</th>
-					<th>Modify</th>
-					<th>Delete</th>
-				</tr>
-			<?php
-				while($row = mysql_fetch_assoc($result))
-				{
-				?>
-				<tr>
-					<td><?php echo $row["Subject_Code"];?></td>
-					<td><?php echo $row["Subject_Title"];?></td>
-					<td><?php echo $row["Subject_Date"];?></td>
-					<td><?php echo $row["Subject_Time"];?></td>
-					<td><a href="modify_sub.php?pid=<?php echo $row['Subject_ID'];?>">Modify</a></td>
-					<td><a href="del_sub.php?pid=<?php echo $row['Subject_ID'];?>"
-			onclick="return confirm('Do you want to delete this subject?')">Delete</a></td>
-				</tr>
+					<td>
+						<p align="center" class="Title">View Subjects</p>
+						<form name="addfrm" method="post" action="">
+							<table width="100%" border="0" align="center" cellpadding="5" cellspacing="1" class="entryTable">
+								<tr>
+									<th>Subject Code</th>
+									<th>Subject Title</th>
+									<th>Subject Date</th>
+									<th>Subject Time</th>
+									<th>Modify</th>
+									<th>Delete</th>
+								</tr>
+								<?php
+									$result = mysql_query("select * from subject");
+									while($row = mysql_fetch_assoc($result))
+									{
+								?>
+								<tr>
+									<td><?php echo $row["Subject_Code"];?></td>
+									<td><?php echo $row["Subject_Title"];?></td>
+									<td><?php echo $row["Subject_Date"];?></td>
+									<td><?php echo $row["Subject_Time"];?></td>
+									<td><a href="modify_sub.php?pid=<?php echo $row['Subject_ID'];?>">Modify</a></td>
+									<td><a href="del_sub.php?pid=<?php echo $row['Subject_ID'];?>"
+							onclick="return confirm('Do you want to delete this subject?')">Delete</a></td>
+								</tr>
 
-			<?php
-				}
-			?>
+								<?php
+									}
+								?>
+							</table>
+						</form>
+					</td>
+				</tr>
 			</table>
+		</div>
+		<div class="clear"></div>
+		<div id="footer">
+
+		</div>
+	</div>
+
+
 </body>
 </html>
