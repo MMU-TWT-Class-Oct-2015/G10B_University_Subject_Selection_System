18912b533a5d3f8c14a677ca65d49f82048c5b57
Tan Jing Xian <1121115994@student.mmu.edu.my>
2016-02-14 17:59:34 +0800

    Update view_sub.php

--- student/view_sub.php
+++ student/view_sub.php
@@ -17,19 +17,21 @@
 </head>
 
 <body>
-	<div id="header">
-		<div id="logo"><a href="index.php"><img src="../picture/logo.png"></a></div>
-		<div id="button">
-
-			<label class="button-style"><a href="index.php">Home</a></label>
-			<label class="button-style"><a href="subject.php">Add Subject</a></label>
-			<label class="button-style"><a href="view_sub.php">View Subject</a></label>
-			<label class="button-style">Welcome, <?php echo $rows["Student_Name"]?> <a href="logout.php">Logout</a></label>
-			<label>&nbsp;</label>
+	<div id="about-bg">
+		<div id="header">
+			<div id="logo"><a href="index.php"><img src="https://cloud.githubusercontent.com/assets/16029083/13032953/e0e6f204-d340-11e5-86a8-6136ec5666ea.png" width="320" height="128"></a></div>
+			<div id="button">
+			
+				<label class="button-style"><a href="index.php">Home</a></label>
+				<label class="button-style"><a href="add_sub.php">Add Subject</a></label>
+				<label class="button-style"><a href="view_sub.php">View Subject</a></label>
+				<label class="button-style">Welcome, <?php echo $rows["Student_Name"]?> <a href="logout.php">Logout</a></label>
+				<label>&nbsp;</label>
+			</div>
 		</div>
 	</div>
 
-		<p style="padding-top:20px;">
+		<p style="padding-top:300px;">
 				<table border="1" width="500px">
 				<tr>
 					<th>Subject Code</th>
